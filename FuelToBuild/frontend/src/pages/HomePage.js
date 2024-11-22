bc4d68db8c7496fdbbf115570a12cb34f1ee4691
Anuththara Perera <anuththaraperera14@gmail.com>
2024-11-22 09:17:01 +0530

    Installed GSAP and added a marquee

--- FuelToBuild/frontend/src/pages/HomePage.js
+++ FuelToBuild/frontend/src/pages/HomePage.js
@@ -1,10 +1,30 @@
 // HomePage.js
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Navbar from '../components/navbar.js';
 import img1 from '../../src/Images/11478256.png';
 import '../../src/pages/HomePage.css'
+import { gsap } from 'gsap';
 
 const HomePage = () => {
+  const marqueeRef = useRef(null);
+
+  useEffect(() => {
+    const marqueeElements = marqueeRef.current.children;
+
+    // Create an infinite scrolling animation
+    gsap.to(marqueeElements, {
+      xPercent: -100,
+      repeat: -1, 
+      duration: 5, 
+      ease: 'linear', 
+      stagger: {
+        each: 0, 
+        repeat: -1,
+      },
+    }).totalProgress(0.5);
+
+  }, []);
+
   return (
     <div>
       <Navbar />
@@ -18,6 +38,69 @@ const HomePage = () => {
           <div className="text-container">
             <h1>Fuel To Build</h1>
           </div>
+          <div className="marquee-container"  ref={marqueeRef}>
+            <div className='marquee-part'>
+            <div className="marquee">
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+          <div className="marquee" >
+            <span>Train Smart</span>
+          </div>
+
+            </div>
+          
+        </div>
       </div>
     </div>
   );
